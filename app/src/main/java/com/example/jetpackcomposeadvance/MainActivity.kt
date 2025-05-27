137081bd13b7bb716def8427fa494399ea1abbb7
M.Kalateh <morteza.kalaateh2105@gmail.com>
2025-05-27 14:31:41 +0330

    first step

---
+++ app/src/main/java/com/example/jetpackcomposeadvance/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.jetpackcomposeadvance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposeadvance.ui.theme.JetpackComposeAdvanceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeAdvanceTheme {
+                ChangeText()
+            }
+        }
+    }
+}
+
+@Composable
+private fun ChangeText() {
+    var text by remember { mutableStateOf("Hello word") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+        Button(onClick = { text = "button change text" }) {
+            Text(text = "change")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeAdvanceTheme {
+    }
+}
