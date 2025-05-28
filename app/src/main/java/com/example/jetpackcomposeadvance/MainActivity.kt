5d8515268b561bae85472b16ec978e7ed04e65d2
M.Kalateh <morteza.kalaateh2105@gmail.com>
2025-05-28 13:46:28 +0330

    ChangeTextTwo method for practice

--- app/src/main/java/com/example/jetpackcomposeadvance/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeadvance/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.jetpackcomposeadvance
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -18,7 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeadvance.ui.theme.JetpackComposeAdvanceTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,6 +50,22 @@ private fun ChangeText() {
         Text(text = text)
         Button(onClick = { text = "button change text" }) {
             Text(text = "change")
+        }
+    }
+}
+
+@Composable
+private fun ChangeTextTwo(initText: String = "Hello world") {
+    var text by remember { mutableStateOf(initText) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text, modifier = Modifier.semantics { contentDescription = "Greeting text" })
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { text = "change text" } , Modifier.semantics { contentDescription = "Change Text button" }) {
+            Text(text = "text was changed")
         }
     }
 }
